d2f965d4395c6a135e11e07afe9b9f322742273a
Poulastha Mukherjee <poulasthamukherjee01@gmail.com>
2024-06-13 16:49:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -98,4 +98,4 @@ def handle_disconnect():
     print(f"{name} has left the room {room}")
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
